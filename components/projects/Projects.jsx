f258364726e990a8b7d924eaa9c828e07c4b82b6
vandilsonbrito <vandilson25@outlook.com>
2024-08-06 11:49:36 -0300

    Feat: add useTranslations hook and its logic

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -2,52 +2,39 @@
 import { useEffect, useState } from 'react';
 import ProjectContainer from '../ProjectContainer/ProjectContainer';
 import data from '../../data.json';
+import {useTranslations} from 'next-intl';
 
 
 export default function Projects() {
 
-    const [reversedProfessionalProjects, setReversedProfessionalProjects] = useState([]);
-    const [reversedPersonalProjects, setReversedPersonalProjects] = useState([]);
-    const [reversedOthersProjects, setReversedOthersProjects] = useState([]);
     const [displayProjects, setDisplayProjects] = useState(1);
     const [loading, setLoading] = useState(true);
+    const t = useTranslations('Home');
 
-    useEffect(() => {
-        setReversedProfessionalProjects(data.projects.professional.reverse());
-        setReversedPersonalProjects(data.projects.personal.reverse());
-        setReversedOthersProjects(data.projects.others.reverse());
-    }, [])
     useEffect(() => {
         if(data) setLoading(false)
     }, [setLoading])
 
     const selectedProjects = {
-        1: reversedProfessionalProjects,
-        2: reversedPersonalProjects,
-        3: reversedOthersProjects
+        1: data.projects['1'],
+        2: data.projects['2']
     }
-
 
     return (
         <section className='w-full h-full flex flex-col justify-center items-center px-7 text-black'>
-            <h2 className='title-h2'>Projetos</h2>
+            <h2 className='title-h2'>{t("Projects.title")}</h2>
 
-            <div className="w-full h-full flex justify-center items-center gap-8 lg:gap-20 mt-16 md:text-xl lg:text-xl 2xl:text-2xl font-medium">
+            <div className="w-fit h-full flex justify-center items-center gap-8 lg:gap-20 mt-16 md:text-xl lg:text-xl 2xl:text-2xl font-medium px-6 py-2 rounded-md shadow-2xl">
                 <button 
                 className={`${displayProjects === 1 ? 'border-b-2 border-greenBlue200' : ''}`} 
                 type="button"
                 onClick={() => setDisplayProjects(1)}
-                >Profissionais</button>
+                > 1 </button>
                 <button 
                 className={`${displayProjects === 2 ? 'border-b-2 border-greenBlue200' : ''}`}
                 type="button"
                 onClick={() => setDisplayProjects(2)}
-                >Pessoais</button>
-                <button 
-                className={`${displayProjects === 3 ? 'border-b-2 border-greenBlue200' : ''}`}
-                type="button"
-                onClick={() => setDisplayProjects(3)}
-                >Outros</button>
+                > 2 </button>
             </div>
 
    
@@ -67,7 +54,7 @@ export default function Projects() {
                                 <ProjectContainer
                                 key={index}
                                 projectName={project.projectName}
-                                description={project.description}
+                                description={t(project.description)}
                                 imageSrc={project.imageSrc}
                                 deployUrl={project.deployUrl}
                                 githubUrl={project.githubUrl}
